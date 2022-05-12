39dd81924c0b4f51976a1f42b750c035e8d4ef9a
John M <johnmabanta51@gmail.com>
2022-05-12 14:31:27 -0600

    Use smart pointers instead

--- src/main.cc
+++ src/main.cc
@@ -1,14 +1,14 @@
 #include <iostream>
+#include <memory>
 
 #include "game.h"
 
-const char* TITLE = "Conway's Game of Life";
+const char* WINDOW_TITLE = "Conway's Game of Life";
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
 
 int main(int argc, char const** argv) {
-  Game* game = new Game(TITLE, WINDOW_WIDTH, WINDOW_HEIGHT);
+  auto game = std::make_unique<Game>(WINDOW_TITLE, WINDOW_WIDTH, WINDOW_HEIGHT);
   game->run();
-  delete game;
   return 0;
 }
